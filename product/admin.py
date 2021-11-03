83081c5ab0da015616394ff4faa3bc493fd58642
Aikol02 <urmatbekovaaiziya@gmail.com>
2021-11-03 16:26:38 +0600

    CRUD, CBV

---
+++ product/admin.py
@@ -0,0 +1,6 @@
+from .models import Category, Product
+
+from django.contrib import admin
+
+admin.site.register(Category)
+admin.site.register(Product)
---
+++ product/urls.py
@@ -0,0 +1,17 @@
+from product.class_views import CategoryListView, ProductDetailView, ProductListView
+from django.urls import path
+
+from .views import *
+
+
+urlpatterns = [
+    path('', CategoryListView.as_view(), name='home'),
+    path('<str:slug>/', ProductListView.as_view(), name='list'),
+    path('product/<int:product_id>/', ProductDetailView.as_view(), name='detail'),
+    path('product/create/', product_create, name='product-create'),
+    path('product/update/<int:product_id>/', product_update,
+                                        name='product-update'),
+                                        path('product/delete/<int:product_id>/', product_delete,
+                                        name='product-delete'),
+]
+
